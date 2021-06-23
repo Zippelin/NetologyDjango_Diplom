6bf56d6e3a46658bb882e05cdf0621cdfc9a15a7
denis <dens.andreev@cti.ru>
2021-06-23 19:05:25 +0300

    added product test

--- shop/tests/products/test_products_view_api.py
+++ shop/tests/products/test_products_view_api.py
@@ -1,6 +1,7 @@
 import pytest
 from rest_framework.reverse import reverse
-from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_403_FORBIDDEN
+from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_403_FORBIDDEN, HTTP_204_NO_CONTENT, \
+    HTTP_401_UNAUTHORIZED
 
 
 @pytest.mark.django_db
@@ -13,13 +14,32 @@ def test_products_list(api_client):
 @pytest.mark.parametrize(
     ["is_superuser", "http_response"],
     (
+        (True, HTTP_200_OK),
+        (False, HTTP_200_OK)
+    )
+)
+@pytest.mark.django_db
+def test_products_details(api_client, products_factory, user_factory, token_factory, is_superuser, http_response):
+    user = user_factory(is_superuser=is_superuser)
+    token = token_factory(user=user)
+    products = products_factory(_quantity=1)
+    url = reverse('product-detail', args=[products[0].id])
+    api_client.credentials(HTTP_AUTHORIZATION='Token ' + token)
+    resp = api_client.get(url)
+    assert resp.status_code == http_response
+
+
+@pytest.mark.parametrize(
+    ["is_superuser", "http_response"],
+    (
         (True, HTTP_201_CREATED),
         (False, HTTP_403_FORBIDDEN)
     )
 )
 @pytest.mark.django_db
-def test_product_post(api_client, products_factory, user_factory, token_factory, is_superuser, http_response):
+def test_post_product(api_client, products_factory, user_factory, token_factory, is_superuser, http_response):
     user = user_factory(is_superuser=is_superuser)
+    token = token_factory(user=user)
     url = reverse('product-list')
     product_payload = {
         "name": "test",
@@ -27,10 +47,54 @@ def test_product_post(api_client, products_factory, user_factory, token_factory,
         "price": "11.0"
     }
 
-    token = token_factory(user=user)
+
     api_client.credentials(HTTP_AUTHORIZATION='Token ' + token)
     resp = api_client.post(url, product_payload, format='json')
     assert resp.status_code == http_response
+
+
+@pytest.mark.parametrize(
+    ["is_superuser", "http_response"],
+    (
+        (True, HTTP_201_CREATED),
+        (False, HTTP_403_FORBIDDEN)
+    )
+)
+@pytest.mark.django_db
+def test_update_product(api_client, products_factory, user_factory, token_factory, is_superuser, http_response):
+    user = user_factory(is_superuser=is_superuser)
+    token = token_factory(user=user)
+    url = reverse('product-list')
+    product_payload = {
+        "name": "test",
+        "description": "text",
+        "price": "11.0"
+    }
+
+
+    api_client.credentials(HTTP_AUTHORIZATION='Token ' + token)
+    resp = api_client.post(url, product_payload, format='json')
+    assert resp.status_code == http_response
+
+
+@pytest.mark.parametrize(
+    ["is_superuser", "http_response"],
+    (
+        (True, HTTP_204_NO_CONTENT),
+        (None, HTTP_401_UNAUTHORIZED)
+    )
+)
+@pytest.mark.django_db
+def test_delete_product(api_client, user_factory, token_factory, products_factory, is_superuser, http_response):
+    user = user_factory(is_superuser=is_superuser)
+    token = token_factory(user=user)
+    product = products_factory(_quantity=1)
+    url = reverse('product-detail', args=[product[0].id])
+
+    api_client.credentials(HTTP_AUTHORIZATION='Token ' + token)
+    resp = api_client.delete(url)
+    assert resp.status_code == http_response
+
 
 
 @pytest.mark.parametrize(
