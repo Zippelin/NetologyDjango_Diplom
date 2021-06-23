03f065f368fc8a02872c1021363503c3f8173502
denis <dens.andreev@cti.ru>
2021-06-23 19:10:42 +0300

    fixed params

--- shop/tests/reviews/test_reviews_view_api.py
+++ shop/tests/reviews/test_reviews_view_api.py
@@ -11,16 +11,16 @@ from rest_framework.status import HTTP_405_METHOD_NOT_ALLOWED, \
 @pytest.mark.parametrize(
     ["is_superuser", "http_response", "results_count"],
     (
-        (False, HTTP_200_OK, 2),
-        (None, HTTP_401_UNAUTHORIZED, 1),
+            (False, HTTP_200_OK, 2),
+            (None, HTTP_401_UNAUTHORIZED, 1),
     )
 )
 @pytest.mark.django_db
-def test_list_review(api_client, user_factory, results_count, reviews_factory, token_factory, is_superuser, http_response):
+def test_list_review(api_client, user_factory, results_count, reviews_factory, token_factory, is_superuser,
+                     http_response):
     user = user_factory(is_superuser=is_superuser)
     token = token_factory(user=user)
     products = reviews_factory(_quantity=results_count)
-
     url = reverse('review-list')
     api_client.credentials(HTTP_AUTHORIZATION='Token ' + token)
     resp = api_client.get(url)
@@ -31,8 +31,8 @@ def test_list_review(api_client, user_factory, results_count, reviews_factory, t
 @pytest.mark.parametrize(
     ["is_superuser", "http_response"],
     (
-        (False, HTTP_201_CREATED),
-        (None, HTTP_401_UNAUTHORIZED),
+            (False, HTTP_201_CREATED),
+            (None, HTTP_401_UNAUTHORIZED),
     )
 )
 @pytest.mark.django_db
@@ -57,8 +57,8 @@ def test_post_review(api_client, user_factory, products_factory, token_factory, 
 @pytest.mark.parametrize(
     ["is_superuser", "http_response"],
     (
-        (False, HTTP_204_NO_CONTENT),
-        (None, HTTP_401_UNAUTHORIZED),
+            (False, HTTP_204_NO_CONTENT),
+            (None, HTTP_401_UNAUTHORIZED),
     )
 )
 @pytest.mark.django_db
@@ -66,7 +66,6 @@ def test_delete_review(api_client, user_factory, reviews_factory, token_factory,
     user = user_factory(is_superuser=is_superuser)
     token = token_factory(user=user)
     review = reviews_factory(_quantity=2, author=user)
-
     url = reverse('review-detail', args=[review[0].id])
     api_client.credentials(HTTP_AUTHORIZATION='Token ' + token)
     resp = api_client.delete(url)
@@ -76,7 +75,7 @@ def test_delete_review(api_client, user_factory, reviews_factory, token_factory,
 @pytest.mark.parametrize(
     ["is_superuser", "http_response"],
     (
-        (False, HTTP_400_BAD_REQUEST),
+            (False, HTTP_400_BAD_REQUEST),
     )
 )
 @pytest.mark.django_db
@@ -103,17 +102,15 @@ def test_add_same_review(api_client, user_factory, products_factory, token_facto
 @pytest.mark.parametrize(
     ["is_superuser", "http_response"],
     (
-        (False, HTTP_405_METHOD_NOT_ALLOWED),
+            (False, HTTP_405_METHOD_NOT_ALLOWED),
     )
 )
 @pytest.mark.django_db
 def test_update_other_review(api_client, user_factory, products_factory, token_factory, is_superuser, http_response):
     user = user_factory(is_superuser=is_superuser)
     dummy_user = user_factory(is_superuser=True)
-
     token = token_factory(user=user)
     dummy_token = token_factory(user=dummy_user)
-
     products = products_factory(_quantity=2)
 
     payload = {
@@ -141,16 +138,16 @@ def test_update_other_review(api_client, user_factory, products_factory, token_f
 
 
 @pytest.mark.parametrize(
-    ["search_field", "search_text", "http_response", "resr_count"],
+    ["search_field", "search_text", "http_response", "resp_count"],
     (
-            ("author", 1, HTTP_200_OK, 2),
+            ("author", "admin", HTTP_200_OK, 2),
             ("product", None, HTTP_200_OK, 1),
             ("date_creation", '2021-06-23', HTTP_200_OK, 2),
     )
 )
 @pytest.mark.django_db
 def test_filters_review(api_client, user_factory, token_factory, reviews_factory, search_field, search_text,
-                          http_response, resr_count):
+                        http_response, resp_count):
     user = user_factory(is_superuser=True)
     token = token_factory(user=user)
     reviews = reviews_factory(_quantity=2, author=user)
@@ -160,4 +157,4 @@ def test_filters_review(api_client, user_factory, token_factory, reviews_factory
     api_client.credentials(HTTP_AUTHORIZATION='Token ' + token)
     resp = api_client.get(url)
     assert resp.status_code == http_response
-    assert len(resp.json()) == resr_count
+    assert len(resp.json()) == resp_count
